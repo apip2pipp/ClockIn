af16ecc6f895543eda1a23cd30309f9c705366fe
rankadian <dianthafafian@gmail.com>
2025-11-23 00:22:18 +0700

    make service for leave request

---
+++ eak_flutter/lib/services/leave_services.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:eak_flutter/config/api_config.dart';
+import 'dart:convert';
+
+class LeaveServices {
+  static Future<Map<String, dynamic>> getLeaveRequests({
+    int page = 1,
+    String? status,
+    String? type,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('auth_token');
+
+    final response = await http.get(
+      Uri.parse("${ApiConfig.baseUrl}/leave-requests"),
+      headers: {
+        "Authorization": "Bearer $token",
+      },
+    );
+
+    return jsonDecode(response.body);
+  }
+
+  static Future<Map<String, dynamic>> submitLeaveRequest({
+    required String type,
+    required DateTime startDate,
+    required DateTime endDate,
+    required String reason,
+    File? attachment,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('auth_token');
+
+    var request = http.MultipartRequest(
+      "POST",
+      Uri.parse("${ApiConfig.baseUrl}/leave-request"),
+    );
+
+    request.headers["Authorization"] = "Bearer $token";
+
+    request.fields["jenis"] = type;
+    request.fields["start_date"] = startDate.toIso8601String();
+    request.fields["end_date"] = endDate.toIso8601String();
+    request.fields["reason"] = reason;
+
+    if (attachment != null) {
+      request.files.add(await http.MultipartFile.fromPath(
+        "attachment",
+        attachment.path,
+      ));
+    }
+
+    final streamed = await request.send();
+    final body = await streamed.stream.bytesToString();
+
+    return jsonDecode(body);
+  }
+}
