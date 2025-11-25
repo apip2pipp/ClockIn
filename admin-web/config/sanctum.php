29c118504b3acebdf6d6b2498ed7e0b33c0e7fee
rankadian <dianthafafian@gmail.com>
2025-11-26 01:19:57 +0700

    delete duplicate middleware

--- admin-web/config/sanctum.php
+++ admin-web/config/sanctum.php
@@ -34,10 +34,6 @@ return [
     */
 
     'guard' => ['web'],
-    'middleware' => [
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
-    ],
 
     /*
     |--------------------------------------------------------------------------
