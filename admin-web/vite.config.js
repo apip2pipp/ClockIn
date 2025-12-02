8c33fdea28a2bd27a59f9ac32ef47e0eeea33366
rankadian <dianthafafian@gmail.com>
2025-12-02 07:53:17 +0700

    configure with ip local

--- admin-web/vite.config.js
+++ admin-web/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
-const currentIP = "192.168.1.32";
+const currentIP = "192.168.1.127";
 
 export default defineConfig({
     plugins: [
