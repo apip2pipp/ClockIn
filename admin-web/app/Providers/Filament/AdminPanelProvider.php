30d90338f53a3ffb842a9e8f52ca85bfaf13f60f
rankadian <dianthafafian@gmail.com>
2025-12-06 18:20:34 +0700

    undo use custom class

--- admin-web/app/Providers/Filament/AdminPanelProvider.php
+++ admin-web/app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             // ->default()
             ->id('admin')
             ->path('admin')
-            ->login(\App\Filament\Pages\Auth\Login::class)
+            ->login()
             ->authGuard('web')
             ->favicon(asset('logo_web.png'))
             ->brandName('ClockIn Admin')
